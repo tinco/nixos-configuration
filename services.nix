fd0a242e711f565e4e8d8463b74d722415992c7c
Tinco Andringa <mail@tinco.nl>
2016-01-23 15:50:54 +0100

    add irssi service

--- services.nix
+++ services.nix
@@ -1,4 +1,14 @@
 {pkgs, ...}:
 
 {
+ systemd.services.ircSession = {
+   wantedBy = [ "multi-user.target" ];
+   after = [ "network.target" ];
+   serviceConfig = {
+     Type = "forking";
+     User = "tinco";
+     ExecStart = ''${pkgs.tmux}/bin/tmux new-session -d -s irc -n irc ${pkgs.irssi}/bin/irssi'';
+     ExecStop = ''${pkgs.tmux}/bin/tmux kill-session -t irc'';
+   };
+ };
 }
