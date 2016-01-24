79e0fb7ce9767586a8e09c59fb32f981bc352da2
Tinco Andringa <mail@tinco.nl>
2016-01-24 19:04:11 +0100

    add reverse proxy service

--- services.nix
+++ services.nix
@@ -11,14 +11,22 @@
      ExecStop = ''${pkgs.tmux}/bin/tmux kill-session -t irc'';
    };
  };
+ systemd.services.httpReverseProxy = {
+   wantedBy = [ "local.target" ];
+   requires = [ "docker.service" ];
+   after = [ "docker.service" ];
+   serviceConfig = {
+     ExecStart = ''${pkgs.bashInteractive}/bin/bash -c "${pkgs.docker}/bin/docker pull jwilder/nginx-proxy:latest && ${pkgs.docker}/bin/docker run --rm --name http_reverse_proxy -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy"'';
+     ExecStop = ''${pkgs.docker}/bin/docker stop -t 2 http_reverse_proxy && ${pkgs.docker}/bin/docker rm http_reverse_proxy'';
+   };
+ };
  systemd.services.tincoNl = {
    wantedBy = [ "local.target" ];
    requires = [ "docker.service" ];
    after = [ "docker.service" ];
    serviceConfig = {
-     User = "tinco";
-     ExecStart = ''${pkgs.bashInteractive}/bin/bash -c "${pkgs.docker}/bin/docker pull tinco/tinco.nl:latest && ${pkgs.docker}/bin/docker run --rm --name tinco.nl -p 80:80 tinco/tinco.nl"'';
-     ExecStop = ''${pkgs.docker}/bin/docker stop -t 2 tinco.nl'';
+     ExecStart = ''${pkgs.bashInteractive}/bin/bash -c "${pkgs.docker}/bin/docker pull tinco/tinco.nl:latest && ${pkgs.docker}/bin/docker run --rm --name tinco.nl -e VIRTUAL_HOST=tinco.nl,www.tinco.nl tinco/tinco.nl"'';
+     ExecStop = ''${pkgs.docker}/bin/docker stop -t 2 tinco.nl && ${pkgs.docker}/bin/docker rm tinco.nl'';
    };
  };
 }
