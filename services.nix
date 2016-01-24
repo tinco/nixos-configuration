1d066d88aa4d67ec7c9266b6abb96f95a3193fbe
Tinco Andringa <mail@tinco.nl>
2016-01-24 01:35:04 +0100

    add tinco.nl service

--- services.nix
+++ services.nix
@@ -11,4 +11,12 @@
      ExecStop = ''${pkgs.tmux}/bin/tmux kill-session -t irc'';
    };
  };
+ systemd.services.tincoNl = {
+   requires = [ "docker.service" ];
+   after = [ "docker.service" ];
+   serviceConfig = {
+     ExecStart = ''${pkgs.docker}/bin/docker pull tinco/tinco.nl:latest && ${pkgs.docker}/bin/docker start -a tinco/tinco.nl'';
+     ExecStop = ''${pkgs.docker}/bin/docker stop -t 2 tinco/tinco.nl'';
+   };
+ };
 }
