f50514824e4f20b9bd65660e5aa678c320864d9b
Tinco Andringa <mail@tinco.nl>
2018-09-06 22:12:14 +0200

    update tinco.nl configuration

--- configuration.nix
+++ configuration.nix
@@ -8,18 +8,15 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./nixos-in-place.nix
+      ./networking.nix
       ./services.nix
-      ./packages.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
-  # boot.loader.grub.device = "/dev/sda";
+  networking.hostName = "tinco"; # Define your hostname.
 
-  networking.hostName = "nixos-1gb-ams3-1"; # Define your hostname.
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 80 443 2221 2222 ];
+  networking.firewall.allowPing = true;
 
   # Select internationalisation properties.
   i18n = {
@@ -38,6 +35,11 @@
     vim
     git
     tmux
+    ruby
+    bundler
+    which
+    autossh
+    irssi
   ];
 
   # List services that you want to enable:
@@ -47,18 +49,65 @@
   services.openssh.permitRootLogin = "no";
   services.openssh.passwordAuthentication = false;
   services.openssh.challengeResponseAuthentication = false;
+  services.openssh.gatewayPorts = "clientspecified";
+  services.openssh.extraConfig = ''
+	AllowAgentForwarding yes
+  '';
+  services.cron.enable = true;
 
   virtualisation.docker.enable = true;
+
+  security.pki.certificates = [
+    ''-----BEGIN CERTIFICATE-----
+MIIEwDCCAqgCCQDGpHCtw9EGZTANBgkqhkiG9w0BAQUFADAiMSAwHgYDVQQDDBdo
+dWIudW5pb25zdGF0aW9uYXBwLmNvbTAeFw0xNTA1MDIwMDU2MDJaFw0xNjA1MDEw
+MDU2MDJaMCIxIDAeBgNVBAMMF2h1Yi51bmlvbnN0YXRpb25hcHAuY29tMIICIjAN
+BgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA1D3VBjtCHVTHj5HQgQrgRYjouiH9
+alZKw2s7Wi3mouwBcEzT8b4bM+s8lsp5q6tL82OAn4dMPxxTJMpwnNH3Vf3+CVx0
+P6DkHkE+DnL0scxWcMgQyf9PfsharDkl5XJFv8jvcahNcSdKsZIu+v9ViaMbpUax
+Zhq4A/pvADFVeEfNvRsUlMdJnzBne8BIWotuK2B1POW8s3bS+Gf+Pe4Qe8lJ9nvW
+Qwd05imceVzOnzSaGH57EwBg+JIO3RlkT2i9m8Y8f+a9Ms5yEPZbKGdLB7nr4/py
+K6B03YroIPMYRJatYwCf0N+iubc/00ecWcRN7ZJJAEuFbPi177OtJagEzsHoFK+k
+3JY96t033gOxgYSCnerqjSn4WAvS3EK+Ss8vSkGbH+wXJXG6I7xDyFtL8ZNDvpWa
+/5WtAM/YFE6Xv7HmATt+GdsOluR5QjzmLDlT8bmOgBrx3dG+RFv0iZrgkc9+EKmX
+198L/jIfIH3eMdwoKXdSYGo5nkWZyDnG2mekSP72yTq5aRQ0H4nFGkKWw7q31tZf
+LHF371WkMmSA2L9Umwp+BOV50tUzpNdp+4t56/HZjM6YNofTnVfcBlswnR0pXZLU
+43Q/k4YkTjv0Z0CQyOL2WasbLFbNwLvGZ6FzvJgY/gjAiOQ9PcnEgtq2vg6eYoeD
+qsNtDCl4I5xNXYMCAwEAATANBgkqhkiG9w0BAQUFAAOCAgEAl5EHn2ny9GtWbeAf
+/J9gqztqJzjB0/5ZsbMvKGLDgOCldtf0AVabr9M1QlzFktJimWnUVbjAMdKj62r1
+ovFtmVtAlOVK/tsgUBISsMNbD+m9OAycCnQOhWr41LcaiHuTYLgVxmGLF9RLmgmI
+yg3evGBxu3yFz0O3UYzjuxKNryovPl6EqsdCZRK7j6U7UMnsvghQ/YEej9DNe+OT
+Ur2Sd0oAN88c9BI1WsOB/pWnwVc44TSBr7QTPfG52/CSn+oxFWD87eBMxrFK3wY+
+pdFO7m/5EsSXJ8nXjr+0Jx/p63YNC1PxkYdkRvzmp6oY50bqrvCCnnKUDnBxqDTd
+Xx4Nb2BknBx2fg2n58RYagPy5SW1Syh7LjoJkFIFs4KhfATfdu3HwxPwVOBuMX8r
+eSHIxu2sEHMTKBTMCx54NAsvmnz+61MSWkCMocQPAcmipCln7uyArmcnxbzJU7gs
+Ke1MsHDg8fansdwnCQBasSwho1qU47gjjogjbgolEz3sPj2+3nBtNUDSCegVtaJX
+u656RVjNH96h3f3iUDczoq/vyKPxrs9CkYprBLZplAwnxpwF0OlbiaR3+Pa9xzVH
+OYtO6+EmaQyS0B+Qm+w43sWMHCmxLz/MfCum+kJxzeDBiRyO3wMUh8eb+pDqwm1L
+p5WwQrH8vW2YS7aiRGTDWZ6CKoQ=
+-----END CERTIFICATE-----
+''
+  ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.tinco = {
     isNormalUser = true;
     uid = 1000;
-    hashedPassword = "$6$qWCDyPsN$nQQH/u1pH0DtREjR5n6nLt/XrKaZ1njEgYvrHSxVi0wvW/Zl8U0rb4Nj69dUzsMqS4F4PHxVQ2oD.vjwFqRz1/";
+    hashedPassword = "***REMOVED***";
     extraGroups = [ "wheel" "networkmanager" "docker" ];
-    openssh.authorizedKeys.keys = [ 
+    openssh.authorizedKeys.keys = [
 	    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDR/fv06LyJW7LMMbSRnP4O9IeP1a+YwnIn9Ja4vJb198udaeosJQB0L2zRjXnFa+z9exmnB9fXvPRVCosMzVzlv6IK647trQo7Wbmh5H6dJUtRDqyPm50yApfs8HUMmpAUT2OjcSJ5N3gV2qXOyj7ccwk5rViDB3o/wfFv3/WCro1t3cL5/0BmvC1DF+WwFdSAoXTIA0j5uOjlc4pneICe8OPfb3GHViKMZBgwbe1lC9L0jJAsYL6vTGeD6jLJJubspRvIe9iSp/2ug7htFTWiJhP7rhtDo33CUcMJHHT0dYBnElqR3Onv4kvSJiQcbR+8hIrJeFSQMqfhjGfC1kNd tinco@phdevbak-1"
 	    "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAwspTqreZr6eLkgLXcI0zhF2HOBuoya0NlUM4dFb1W5kk4OfeZa3sT1yQbRCZHcoVtJrk9JLe2v7xrr8g7+rSJJKXX5VaxgqB3YGDOg8t11XZG4pA65kl4VZ0gEFXE6xYYgODHuLgHF9DMeDGRGCVuBwIDcXxq279k8YE2e2dObkmctYNcQk7LJ+NHAQSmUMIKcRAvnncWq7XPBEQ0xSlW+yp7QJ36C1hLxk7fupVb64qmxsFsuGwE7qBbjDG/7F9gzDAc9gHBLEc7xCEHf1FTf9TjbhevtaTrJ0otATcjQccIP4/BHdpRLbXEkvUljgK6RRzxsRGo+udXLokkE5XWw== tinco@macbook"
+    ];
+  };
+
+  users.extraUsers.hongli = {
+    isNormalUser = true;
+    uid = 1001;
+    initialHashedPassword = "***REMOVED***";
+    extraGroups = [ "docker" ];
+    openssh.authorizedKeys.keys = [
+	    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC25pwlRmDOdB9D1a1xWZBLqNo7PwIt1vEuM8Y298ZoLeXx0iOOoI6qxcBmeMZJJZ3v9rrtMD7zQ7ijg5JMa19cSx4MnxAGes/QjSso2SfbKPuKPRCvRNU6tNZ5vRKNJZxX3EmxeEjTkhKaF0kK1ja6cDEG9O3ldV28kHqcNXOVjBTzqLTfll2+XnXRUWAu0PBpzwhA1hsgzbofGWsYyclOTHk4Vjwb8u9EaUQPo40im7H/q/zATZi+S1oUxsnuMPdEGKVSNnrWZOQFvYY17EzJOnIwdwfCQjVoXJZzArlF46KVQwXvbcpkHga5KxusbGwZWbrt7ZgFuzgpRf62Ty/r hongli@hongli-lais-macbook-pro.local"
     ];
   };
 
--- services.nix
+++ services.nix
@@ -1,4 +1,85 @@
 {pkgs, ...}:
 
+let mkDockerService = {container, name, env}: {
+  wantedBy = [ "multi-user.target" ];
+  requires = [ "docker.service" ];
+  after = [ "docker.service" ];
+  serviceConfig = {
+    AutoRestart = "Always";
+    ExecStart = ''\
+	${pkgs.bashInteractive}/bin/bash -c "${pkgs.docker}/bin/docker rm ${name} ;\
+	${pkgs.docker}/bin/docker pull ${container} && \
+	${pkgs.docker}/bin/docker run --rm --name ${name} ${env} ${container}"'';
+    ExecStop = ''\
+	${pkgs.bashInteractive}/bin/bash -c "${pkgs.docker}/bin/docker stop -t 2 ${name} &&\
+	${pkgs.docker}/bin/docker rm ${name}"'';
+  };
+};
+in
 {
+ systemd.services.ircSession = {
+   wantedBy = [ "multi-user.target" ];
+   after = [ "network.target" ];
+   serviceConfig = {
+     Type = "forking";
+     User = "tinco";
+     ExecStart = ''${pkgs.tmux}/bin/tmux new-session -d -s irc -n irc ${pkgs.irssi}/bin/irssi'';
+     ExecStop = ''${pkgs.tmux}/bin/tmux kill-session -t irc'';
+   };
+ };
+
+ systemd.services.httpReverseProxy = mkDockerService { 
+   container = "jwilder/nginx-proxy:0.2.0" ;
+   name = "http_reverse_proxy" ;
+   env = "-p 80:80 -e DEFAULT_HOST=www.tinco.nl -v /var/run/docker.sock:/tmp/docker.sock:ro";
+ };
+
+ systemd.services.stellarCrawler = mkDockerService { 
+   container = "tinco/stellar-crawler:latest" ;
+   name = "stellar-crawler" ;
+   env = "-v /data/stellar-crawler:/app/data";
+ };
+
+ systemd.services.tincoNl = mkDockerService {
+   container = "tinco/tinco.nl:latest" ;
+   name = "tinco.nl" ;
+   env = "-e VIRTUAL_HOST=www.tinco.nl,tinco.nl";
+ };
+
+ systemd.services.quorumExplorer = mkDockerService {
+   container = "tinco/quorum_explorer:latest" ;
+   name = "quorum_explorer" ;
+   env = "-v /data/stellar-crawler:/usr/share/nginx/html/data -e VIRTUAL_HOST=www.quorumexplorer.com,quorumexplorer.com";
+ };
+
+ systemd.services.quorumExplorerAPI = mkDockerService {
+   container = "nginx:latest" ;
+   name = "quorum_explorer_api" ;
+   env = "-v /data/stellar-crawler/api:/usr/share/nginx/html -e VIRTUAL_HOST=api.quorumexplorer.com";
+ };
+
+ systemd.services.blogTincoNl = mkDockerService {
+   container = "tinco/blog.tinco.nl:latest" ;
+   name = "blog.tinco.nl" ;
+   env = "-e VIRTUAL_HOST=blog.tinco.nl";
+ };
+
+ systemd.services.haskellToolbox = mkDockerService {
+   container = "tinco/haskell-toolbox:latest" ;
+   name = "haskell-toolbox.com" ;
+   env = "-e VIRTUAL_HOST=haskell-toolbox.com";
+ };
+
+ systemd.services.cosmicUCom = mkDockerService {
+   container = "tinco/cosmic-u.com:latest" ;
+   name = "cosmic-u.com" ;
+   env = "-e VIRTUAL_HOST=www.cosmic-u.com,cosmic-u.com";
+ };
+
+ systemd.services.httpShares = mkDockerService {
+   container = "nginx:latest" ;
+   name = "shares.tinco.nl" ;
+   env = "-e VIRTUAL_HOST=shares.tinco.nl -v /home/tinco/shares:/usr/share/nginx/html:ro";
+ };
+
 }
